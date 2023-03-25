ae64258012031e4c8d96ed99778af9e947e46072
Ninad Sonawane <128928019+Ninad-Sonawane@users.noreply.github.com>
2023-03-26 00:01:04 +0530

    Add files via upload
    This is First Java " Hello World " code

---
+++ Print_Hello.java
@@ -0,0 +1,34 @@
+package com.basic;
+
+import java.util.Scanner;
+
+public class Print_Hello {
+
+	public static void main(String[] args) {
+		
+		//--------------1------------//
+		/*System.out.println("Hello");
+		System.out.println("NINAD SONAWANE");
+		*/
+		//--------------2------------//
+		//System.out.println("Hello\nNINAD SONAWANE");
+		
+		//--------------3------------//
+		// Sample solution using input from the user:
+		
+		/*Scanner input = new Scanner (System.in);
+		System.out.println("Input Your First Name: ");
+		String aname = (input.next());
+		System.out.println("Input Your Last Name: ");
+		String bname = (input.next());
+		System.out.println();
+		System.out.println("Hello\n"+ aname+" "+ bname);*/
+		
+		
+		
+		
+				
+
+	}
+
+}
